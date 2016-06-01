7a2334105d9bdda7caa299333b07383dd6db70cf
David <david.baron@softtek.com>
2016-05-31 20:30:49 -0500

    added ex1

---
+++ 1/main.js
@@ -0,0 +1,9 @@
+var suma = 0;
+do {
+	var ingreso = prompt("ingresa un número. Escribe 'fin' para salir");
+} while (ingreso != "fin");
+
+console.log(suma);
+
+/*NOTA: El algoritmo funciona según las instrucciones de la 
+pregunta, sale del ciclo solo si se escribe la palabra 'fin',*/
